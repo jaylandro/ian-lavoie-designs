2789daaa82ae1bfdce45f7d15602ca83a45600d1
Jay Landro <jaylandro@gmail.com>
2020-04-20 08:44:21 -0700

    Use cloudinary and fix lightbox

---
+++ _data/cloudinaryFiles.js
@@ -0,0 +1,14 @@
+var cloudinary = require('cloudinary').v2;
+
+module.exports = () => {
+return cloudinary.search
+    .expression('folder:ianlavoiedesigns')
+    .execute()
+    .then(result => {
+        let filenames = result.resources.map(file => `${file.filename}.${file.format}`)
+
+        console.log("Website photos", filenames);
+        return filenames
+    })
+    .catch(err => console.error(err));
+}
