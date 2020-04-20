3721c3b15d4b98acd35892b2417340bf3b98b830
Jay Landro <jaylandro@gmail.com>
2020-04-20 09:28:06 -0700

    Sort by filename

--- _data/cloudinaryFiles.js
+++ _data/cloudinaryFiles.js
@@ -3,6 +3,7 @@ var cloudinary = require('cloudinary').v2;
 module.exports = () => {
 return cloudinary.search
     .expression('folder:ianlavoiedesigns')
+    .sort_by('filename','asc')
     .execute()
     .then(result => {
         let filenames = result.resources.map(file => `${file.filename}.${file.format}`)
